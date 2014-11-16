26561990c0d70ede48b2348d70689a31df3340a7
Tod Fitch <Tod@FitchDesign.com>
2014-11-16 14:25:09 -0800

    Initial settings/configuration menu with download button
    Signed-off-by: Tod Fitch <Tod@FitchDesign.com>

--- src/org/fitchfamily/android/gsmlocation/settings.java
+++ src/org/fitchfamily/android/gsmlocation/settings.java
@@ -2,51 +2,26 @@ package org.fitchfamily.android.gsmlocation;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import java.util.Date;
-import android.util.Log;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
-public class settings extends Activity implements View.OnClickListener {
-    protected String TAG = "gsm-backend-settings";
-    private static boolean DEBUG = true;
+public class settings extends Activity {
 
-    Button btn;
-    CheckBox useOCI;
-    CheckBox useMLS;
+    class PrefsFragment extends PreferenceFragment {
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "Starting settings");
-        setContentView(R.layout.settings);
-
-        btn = (Button) findViewById(R.id.downloadButton);
-        useOCI = (CheckBox)findViewById(R.id.useOCI);
-        useMLS = (CheckBox)findViewById(R.id.useMLS);
-
-        btn.setOnClickListener(this);
-    }
-
-    public void onClick(View view) {
-        downloadData();
-    }
-
-    private void downloadData() {
-//        btn.setText(new Date().toString());
-//        Log.d(TAG, "updating time");
-
-        String lg = "";
-        if (useOCI.isChecked())
-            lg += "Use OCI.  ";
-        else
-            lg += "Don't use OCI.  ";
-        if (useMLS.isChecked())
-            lg += "Use MLS.  ";
-        else
-            lg += "Don't use MLS.  ";
-        if (DEBUG) Log.d(TAG, lg);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                                                        new PrefsFragment()).commit();
     }
 }
-
