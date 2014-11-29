1e6f92bae2346c50d9845e9c40b4cb3686751fb0
Tod Fitch <Tod@FitchDesign.com>
2014-11-29 07:28:53 -0800

    Show date of current database in settings
    Signed-off-by: Tod Fitch <Tod@FitchDesign.com>

--- src/org/fitchfamily/android/gsmlocation/prefsFragment.java
+++ src/org/fitchfamily/android/gsmlocation/prefsFragment.java
@@ -1,5 +1,10 @@
 package org.fitchfamily.android.gsmlocation;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.File;
+
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
@@ -58,6 +63,30 @@ public class prefsFragment extends PreferenceFragment {
             });
         } else {
             if (DEBUG) Log.d(TAG, "prefsFragment.onCreate(): mccFilterPreference is null");
+        }
+
+        EditTextPreference currentDbPreference = (EditTextPreference) this.findPreference("db_date_preference");
+        if (currentDbPreference != null) {
+            if(currentDbPreference.getText()==null) {
+                // to ensure we don't get a null value
+                // set first value by default
+                mccFilterPreference.setText("");
+            }
+
+            String currentDbInfo = "";
+            if (appConstants.DB_FILE.exists()) {
+                if (appConstants.DB_FILE.canRead()) {
+                    DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
+                    currentDbInfo = "Last Modified: " + dateTimeInstance.format(appConstants.DB_FILE.lastModified());
+                } else {
+                    currentDbInfo = "No read permission on database.";
+                }
+            } else {
+                currentDbInfo = "No database file found.";
+            }
+            currentDbPreference.setSummary(currentDbInfo);
+        } else {
+            if (DEBUG) Log.d(TAG, "prefsFragment.onCreate(): currentDbPreference is null");
         }
 
     }
